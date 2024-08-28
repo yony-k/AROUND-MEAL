21cbf841b58e5389dbf3d7fe40a68ca5c8ff5eb9
Sona <objet_an@naver.com>
2024-08-28 11:12:12 +0900

    [FEAT] 평가 엔티티 설계

---
+++ src/main/java/com/lucky/around/meal/entity/Rating.java
@@ -0,0 +1,41 @@
+package com.lucky.around.meal.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rating {
+/*
+    유저(member) fk 평가를 생성한 유저 FK
+    맛집(restaurant) fk 유저가 평가한 대상 맛집 FK
+    점수(score) integer 0 ~ 5 에 해당하는 점수 [DTO,SERVICE]
+    내용(content) string 평가 내용이며 0~255자 이내 작성 [DTO,SERVICE]
+*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    private Integer score;
+    private String content;
+
+    @Builder
+    public Rating(Member member, Restaurant restaurant, Integer score, String content){
+        this.member = member;
+        this.restaurant = restaurant;
+        this.score = score;
+        this.content = content;
+    }
+}
