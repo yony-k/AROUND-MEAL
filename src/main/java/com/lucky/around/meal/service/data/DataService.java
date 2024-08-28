6d3391aee510b44d3e320a51da730d7ecd1dcfd2
Hajin <qwertygoov@naver.com>
2024-08-28 20:37:52 +0900

    [FEAT] 데이터 파이프라인 구현

--- src/main/java/com/lucky/around/meal/service/data/DataService.java
+++ src/main/java/com/lucky/around/meal/service/data/DataService.java
@@ -1,8 +1,6 @@
 package com.lucky.around.meal.service.data;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -46,6 +44,12 @@ public class DataService {
   @Value("${API_FORMAT_TYPE}")
   private String FORMAT_TYPE;
 
+  @Value("${API_DATA_SIZE}")
+  private int DATA_SIZE;
+
+  @Value("${API_PAGE_SIZE}")
+  private int PAGE_SIZE;
+
   @Autowired
   public DataService(
       WebClient.Builder webClientBuilder,
@@ -59,19 +63,23 @@ public class DataService {
   }
 
   // 데이터 수집
-  public Mono<String> fetchData() {
-    String startIndex = "1";
-    String endIndex = "20";
-    String uri =
-        String.format("/%s/%s/%s/%s/%s", KEY, FORMAT_TYPE, SERVICE_NAME, startIndex, endIndex);
+  public Mono<String> fetchData(int startIndex, int endIndex) {
+    try {
+      String uri =
+          String.format("/%s/%s/%s/%s/%s", KEY, FORMAT_TYPE, SERVICE_NAME, startIndex, endIndex);
 
-    WebClient webClient = webClientBuilder.baseUrl(BASE_URL).build();
+      WebClient webClient = webClientBuilder.baseUrl(BASE_URL).build();
 
-    return webClient
-        .get()
-        .uri(uriBuilder -> uriBuilder.path(uri).build())
-        .retrieve()
-        .bodyToMono(String.class);
+      return webClient
+          .get()
+          .uri(uriBuilder -> uriBuilder.path(uri).build())
+          .retrieve()
+          .bodyToMono(String.class);
+    } catch (Exception e) {
+      log.error("[fetchData] error - " + e);
+    }
+
+    return null;
   }
 
   // 데이터 파싱
@@ -85,21 +93,22 @@ public class DataService {
           .map(
               rowNode -> {
                 Map<String, String> parsedData = new HashMap<>();
-                parsedData.put("id", rowNode.path("MGTNO").asText());
-                parsedData.put("restaurantName", rowNode.path("BPLCNM").asText());
-                parsedData.put("category", rowNode.path("UPTAENM").asText());
-                parsedData.put("restaurantTel", rowNode.path("SITETEL").asText());
-                parsedData.put("x", rowNode.path("X").asText());
-                parsedData.put("y", rowNode.path("Y").asText());
+                //                parsedData.put("id", rowNode.path("MGTNO").asText());
+                parsedData.put("id", getOrEmpty(rowNode, "MGTNO"));
+                parsedData.put("restaurantName", getOrEmpty(rowNode, "BPLCNM"));
+                parsedData.put("category", getOrEmpty(rowNode, "UPTAENM"));
+                parsedData.put("restaurantTel", getOrEmpty(rowNode, "SITETEL"));
+                parsedData.put("x", getOrEmpty(rowNode, "X"));
+                parsedData.put("y", getOrEmpty(rowNode, "Y"));
 
-                String jibunAddress = rowNode.path("SITEWHLADDR").asText();
-                String[] jibunAddresses = jibunAddress.split(" ", 3);
+                String jibunAddress = getOrEmpty(rowNode, "SITEWHLADDR");
+                String[] jibunAddresses = splitAddress(jibunAddress);
                 parsedData.put("dosi", jibunAddresses[0]);
                 parsedData.put("sigungu", jibunAddresses[1]);
                 parsedData.put("jibunDetailAddress", jibunAddresses[2]);
 
-                String doroAddress = rowNode.path("RDNWHLADDR").asText();
-                String[] doroAddresses = doroAddress.split(" ", 3);
+                String doroAddress = getOrEmpty(rowNode, "RDNWHLADDR");
+                String[] doroAddresses = splitAddress(doroAddress);
                 parsedData.put("doroDetailAddress", doroAddresses[2]);
 
                 return parsedData;
@@ -107,29 +116,56 @@ public class DataService {
           .collect(Collectors.toList());
 
     } catch (Exception e) {
+      log.info("[parseData] error - " + e);
       throw new CustomException(DataExceptionType.DATA_PARSING_FAILED);
     }
   }
 
+  // 데이터가 있으면 그대로 반환, 없으면 "" 반환
+  private String getOrEmpty(JsonNode node, String field) {
+    String value = node.path(field).asText("");
+
+    if (value.isEmpty()) {
+      log.error(
+          "node - " + node.path("MGTNO").asText("") + ", filed: " + field + ", value: " + value);
+    }
+
+    return value.isEmpty() ? "" : value;
+  }
+
+  // 주소 분할
+  private String[] splitAddress(String address) {
+    log.info("splitedAddress - " + Arrays.stream(new String[] {address}).toList());
+
+    if (address == null || address.isEmpty()) {
+      return new String[] {"", "", ""};
+    }
+    return address.split(" ", 3);
+  }
+
   // 데이터 저장
   public void saveData(List<Map<String, String>> parsedDataList) {
-    for (Map<String, String> parsedData : parsedDataList) {
-      Region region = saveRegion(parsedData.get("dosi"), parsedData.get("sigungu"));
+    try {
+      for (Map<String, String> parsedData : parsedDataList) {
+        Region region = saveRegion(parsedData.get("dosi"), parsedData.get("sigungu"));
 
-      Restaurant restaurant =
-          Restaurant.builder()
-              .id(parsedData.get("id"))
-              .restaurantName(parsedData.get("restaurantName"))
-              .restaurantTel(parsedData.get("restaurantTel"))
-              .jibunDetailAddress(parsedData.get("jibunDetailAddress"))
-              .doroDetailAddress(parsedData.get("doroDetailAddress"))
-              .region(region)
-              .category(Category.of(parsedData.get("category")))
-              .lon(validateCoordinate(parsedData.get("x")))
-              .lat(validateCoordinate(parsedData.get("y")))
-              .build();
+        Restaurant restaurant =
+            Restaurant.builder()
+                .id(parsedData.get("id"))
+                .restaurantName(parsedData.get("restaurantName"))
+                .restaurantTel(parsedData.get("restaurantTel"))
+                .jibunDetailAddress(parsedData.get("jibunDetailAddress"))
+                .doroDetailAddress(parsedData.get("doroDetailAddress"))
+                .region(region)
+                .category(Category.of(parsedData.get("category")))
+                .lon(validateCoordinate(parsedData.get("x")))
+                .lat(validateCoordinate(parsedData.get("y")))
+                .build();
 
-      restaurantRepository.save(restaurant);
+        restaurantRepository.save(restaurant);
+      }
+    } catch (Exception e) {
+      log.error("[saveData] error - " + e);
     }
   }
 
@@ -154,16 +190,30 @@ public class DataService {
 
   // 스케줄러 설정
   //  @Scheduled(cron = "0 0 1 * * ?") // 매일 오전 1시 실행
-  @Scheduled(fixedRate = 180_000) // 테스트용 3분 마다 실행
+  @Scheduled(fixedRate = 90_000) // 테스트용 2분 마다 실행
   public void executeDataPipeline() {
     try {
       log.info("[executeDataPipeline] started!");
-      String responseDta = fetchData().block();
-      List<Map<String, String>> parsedDataList = parseData(responseDta);
-      saveData(parsedDataList);
+
+      int startIndex = 1;
+      boolean hasMoreData = true;
+
+      while (hasMoreData) {
+        int endIndex = startIndex + PAGE_SIZE - 1;
+        String responseDta = fetchData(startIndex, endIndex).block();
+        List<Map<String, String>> parsedDataList = parseData(responseDta);
+
+        if (parsedDataList.isEmpty()) {
+          hasMoreData = false;
+        } else {
+          saveData(parsedDataList);
+          startIndex += PAGE_SIZE;
+        }
+      }
+
       log.info("[executeDataPipeline] completed!");
     } catch (Exception e) {
-      log.error("[executeDataPipeline] error - " + e.getMessage());
+      log.error("[executeDataPipeline] error - " + e);
       throw new CustomException(DataExceptionType.SCHEDULING_FAILED);
     }
   }
