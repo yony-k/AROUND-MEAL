c7b8fd6cdcc4e856e4b7f267ec8f7374f067b045
Hajin <qwertygoov@naver.com>
2024-08-28 19:01:26 +0900

    [FEAT] 데이터 배열로 가져오기 및 좌표 값 유효성 검사

--- src/main/java/com/lucky/around/meal/service/data/DataService.java
+++ src/main/java/com/lucky/around/meal/service/data/DataService.java
@@ -1,7 +1,10 @@
 package com.lucky.around.meal.service.data;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -58,7 +61,7 @@ public class DataService {
   // 데이터 수집
   public Mono<String> fetchData() {
     String startIndex = "1";
-    String endIndex = "1";
+    String endIndex = "20";
     String uri =
         String.format("/%s/%s/%s/%s/%s", KEY, FORMAT_TYPE, SERVICE_NAME, startIndex, endIndex);
 
@@ -72,54 +75,71 @@ public class DataService {
   }
 
   // 데이터 파싱
-  private Map<String, String> parseData(String data) {
+  private List<Map<String, String>> parseData(String data) {
     try {
       JsonNode rootNode = objectMapper.readTree(data);
-      JsonNode rowNode = rootNode.path(SERVICE_NAME).path("row").get(0);
+      JsonNode rowNodes = rootNode.path(SERVICE_NAME).path("row");
 
       // 필요한 데이터 추출 후, Map 으로 만들기
-      Map<String, String> parsedData = new HashMap<>();
-      parsedData.put("id", rowNode.path("MGTNO").asText());
-      parsedData.put("restaurantName", rowNode.path("BPLCNM").asText());
-      parsedData.put("category", rowNode.path("UPTAENM").asText());
-      parsedData.put("restaurantTel", rowNode.path("SITETEL").asText());
-      parsedData.put("x", rowNode.path("X").asText());
-      parsedData.put("y", rowNode.path("Y").asText());
+      return StreamSupport.stream(rowNodes.spliterator(), false)
+          .map(
+              rowNode -> {
+                Map<String, String> parsedData = new HashMap<>();
+                parsedData.put("id", rowNode.path("MGTNO").asText());
+                parsedData.put("restaurantName", rowNode.path("BPLCNM").asText());
+                parsedData.put("category", rowNode.path("UPTAENM").asText());
+                parsedData.put("restaurantTel", rowNode.path("SITETEL").asText());
+                parsedData.put("x", rowNode.path("X").asText());
+                parsedData.put("y", rowNode.path("Y").asText());
 
-      String jibunAddress = rowNode.path("SITEWHLADDR").asText();
-      String[] jibunAddresses = jibunAddress.split(" ", 3);
-      parsedData.put("dosi", jibunAddresses[0]);
-      parsedData.put("sigungu", jibunAddresses[1]);
-      parsedData.put("jibunDetailAddress", jibunAddresses[2]);
+                String jibunAddress = rowNode.path("SITEWHLADDR").asText();
+                String[] jibunAddresses = jibunAddress.split(" ", 3);
+                parsedData.put("dosi", jibunAddresses[0]);
+                parsedData.put("sigungu", jibunAddresses[1]);
+                parsedData.put("jibunDetailAddress", jibunAddresses[2]);
 
-      String doroAddress = rowNode.path("RDNWHLADDR").asText();
-      String[] doroAddresses = doroAddress.split(" ", 3);
-      parsedData.put("doroDetailAddress", doroAddresses[2]);
+                String doroAddress = rowNode.path("RDNWHLADDR").asText();
+                String[] doroAddresses = doroAddress.split(" ", 3);
+                parsedData.put("doroDetailAddress", doroAddresses[2]);
 
-      return parsedData;
+                return parsedData;
+              })
+          .collect(Collectors.toList());
+
     } catch (Exception e) {
       throw new CustomException(DataExceptionType.DATA_PARSING_FAILED);
     }
   }
 
   // 데이터 저장
-  public void saveData(Map<String, String> parsedData) {
-    Region region = saveRegion(parsedData.get("dosi"), parsedData.get("sigungu"));
+  public void saveData(List<Map<String, String>> parsedDataList) {
+    for (Map<String, String> parsedData : parsedDataList) {
+      Region region = saveRegion(parsedData.get("dosi"), parsedData.get("sigungu"));
 
-    Restaurant restaurant =
-        Restaurant.builder()
-            .id(parsedData.get("id"))
-            .restaurantName(parsedData.get("restaurantName"))
-            .restaurantTel(parsedData.get("restaurantTel"))
-            .jibunDetailAddress(parsedData.get("jibunDetailAddress"))
-            .doroDetailAddress(parsedData.get("doroDetailAddress"))
-            .region(region)
-            .category(Category.of(parsedData.get("category")))
-            .lon(Double.parseDouble(parsedData.get("x")))
-            .lat(Double.parseDouble(parsedData.get("y")))
-            .build();
+      Restaurant restaurant =
+          Restaurant.builder()
+              .id(parsedData.get("id"))
+              .restaurantName(parsedData.get("restaurantName"))
+              .restaurantTel(parsedData.get("restaurantTel"))
+              .jibunDetailAddress(parsedData.get("jibunDetailAddress"))
+              .doroDetailAddress(parsedData.get("doroDetailAddress"))
+              .region(region)
+              .category(Category.of(parsedData.get("category")))
+              .lon(validateCoordinate(parsedData.get("x")))
+              .lat(validateCoordinate(parsedData.get("y")))
+              .build();
 
-    restaurantRepository.save(restaurant);
+      restaurantRepository.save(restaurant);
+    }
+  }
+
+  // "" 값 일 때, 0.0 넣어주기
+  private Double validateCoordinate(String coordinate) {
+    if (coordinate == null || coordinate.trim().isEmpty()) {
+      return 0.0;
+    }
+
+    return Double.parseDouble(coordinate);
   }
 
   private Region saveRegion(String dosi, String sigungu) {
@@ -133,14 +153,14 @@ public class DataService {
   }
 
   // 스케줄러 설정
-  //  @Scheduled(cron = "0 0 1 * * ?") // 매일 오전 1신
+  //  @Scheduled(cron = "0 0 1 * * ?") // 매일 오전 1시 실행
   @Scheduled(fixedRate = 180_000) // 테스트용 3분 마다 실행
   public void executeDataPipeline() {
     try {
       log.info("[executeDataPipeline] started!");
       String responseDta = fetchData().block();
-      Map<String, String> parsedData = parseData(responseDta);
-      saveData(parsedData);
+      List<Map<String, String>> parsedDataList = parseData(responseDta);
+      saveData(parsedDataList);
       log.info("[executeDataPipeline] completed!");
     } catch (Exception e) {
       log.error("[executeDataPipeline] error - " + e.getMessage());
