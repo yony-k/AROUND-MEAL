2f81da1d47bcdee6f3901f2d73f73f5e533dd76f
Sona <objet_an@naver.com>
2024-08-28 17:02:22 +0900

    [FEAT] RegionRepository 추가

---
+++ src/main/java/com/lucky/around/meal/repository/RegionRepository.java
@@ -0,0 +1,7 @@
+package com.lucky.around.meal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucky.around.meal.entity.Region;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {}
