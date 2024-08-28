c08408605080680684c7ced3c9bc5f067a066411
sung-silver <jinna0319@gmail.com>
2024-08-28 09:50:05 +0900

    [FEAT] 서울시 공공데이터에 맞추어 업태구분명을 참고해 Category Enum 구현

--- src/main/java/com/lucky/around/meal/entity/enums/Category.java
+++ src/main/java/com/lucky/around/meal/entity/enums/Category.java
@@ -6,17 +6,25 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Category {
-  KOREAN("한식"),
-  CHINESE("중식"),
-  JAPANESE("일식"),
-  WESTERN("양식"),
-  CHICKEN("치킨"),
-  PIZZA("피자"),
-  FASTFOOD("패스트푸드"),
-  CAFE("카페"),
-  DESSERT("디저트"),
-  SNACK("분식"),
-  BAKERY("베이커리"),
+  KOREAN_FOOD("한식"),
+  JAPANESE_FOOD("일식"),
+  CHINESE_FOOD("중국식"),
+  FAMILY_RESTAURANT("패밀리레스토랑"),
+  CHICKEN_AND_BEER("호프/통닭"),
+  SOJU_BAR("정종/대포집/소주방"),
+  BBQ("식육(숯불구이)"),
+  INTERNATIONAL_CUISINE("외국음식전문점(인도,태국등)"),
+  SNACK_BAR("분식"),
+  SENSUAL_BAR("감성주점"),
+  SUSHI_BAR("횟집"),
+  KIDS_CAFE("키즈카페"),
+  WESTERN_FOOD("경양식"),
+  LIVE_CAFE("라이브카페"),
+  BUFFET("뷔페식"),
+  CAFE("까페"),
+  KIMBAP("김밥(도시락)"),
+  COLD_NOODLE("냉면집"),
+  CATERING("출장조리"),
   ETC("기타");
 
   private String name;
