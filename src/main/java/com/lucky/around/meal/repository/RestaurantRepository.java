aadf8f54c981f0d6fcb1b46611963c45c905c2ac
sung-silver <jinna0319@gmail.com>
2024-08-29 15:12:37 +0900

    [FEAT] RestaurantRepository -  findRestaurantsWithinRangeByDistance, findRestaurantsWithinRangeByRating 구현

--- src/main/java/com/lucky/around/meal/repository/RestaurantRepository.java
+++ src/main/java/com/lucky/around/meal/repository/RestaurantRepository.java
@@ -1,7 +1,25 @@
 package com.lucky.around.meal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lucky.around.meal.entity.Restaurant;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, String> {}
+public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
+  @Query(
+      "SELECT r FROM Restaurant r "
+          + "WHERE ST_Distance(ST_SetSRID(ST_MakePoint(r.lon, r.lat), 4326), ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) <= :range "
+          + "ORDER BY ST_Distance(ST_SetSRID(ST_MakePoint(r.lon, r.lat), 4326), ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) ASC")
+  List<Restaurant> findRestaurantsWithinRangeByDistance(
+      @Param("lat") double lat, @Param("lon") double lon, @Param("range") double range);
+
+  @Query(
+      "SELECT r FROM Restaurant r "
+          + "WHERE ST_Distance(ST_SetSRID(ST_MakePoint(r.lon, r.lat), 4326), ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) <= :range "
+          + "ORDER BY r.ratingAverage DESC")
+  List<Restaurant> findRestaurantsWithinRangeByRating(
+      @Param("lat") double lat, @Param("lon") double lon, @Param("range") double range);
+}
