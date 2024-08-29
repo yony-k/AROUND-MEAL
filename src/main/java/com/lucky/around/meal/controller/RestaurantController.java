b8075fa271e6d561f65fc99c1dbfcb313c5c4df1
SeoJeongYu <bbwest0709@gmail.com>
2024-08-29 20:49:36 +0900

    [FIX] 문법 오류 수정 및 정리

--- src/main/java/com/lucky/around/meal/controller/RestaurantController.java
+++ src/main/java/com/lucky/around/meal/controller/RestaurantController.java
@@ -1,21 +1,14 @@
 package com.lucky.around.meal.controller;
 
+import java.util.*;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lucky.around.meal.controller.response.RestaurantDetailResponseDto;
-import com.lucky.around.meal.repository.RestaurantRepository;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.lucky.around.meal.controller.dto.GetRestaurantsDto;
+import com.lucky.around.meal.controller.response.RestaurantDetailResponseDto;
+import com.lucky.around.meal.repository.RestaurantRepository;
 import com.lucky.around.meal.service.RestaurantService;
 
 import lombok.RequiredArgsConstructor;
@@ -35,8 +28,9 @@ public class RestaurantController {
     RestaurantDetailResponseDto restaurantDetail =
         restaurantService.getRestaurantDetail(restaurantId);
     return ResponseEntity.ok(restaurantDetail);
+  }
 
-  @GetMapping
+  @GetMapping()
   public List<GetRestaurantsDto> findRestaurantsWithinRange(
       final @RequestParam double lat,
       final @RequestParam double lon,
