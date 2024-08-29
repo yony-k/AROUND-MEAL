5d9f5e31b78d1180483f4e9ee084ac34fdd859a9
sung-silver <jinna0319@gmail.com>
2024-08-29 15:13:54 +0900

    [FEAT] RestaurantRepository에 final 키워드 추가

--- src/main/java/com/lucky/around/meal/repository/RestaurantRepository.java
+++ src/main/java/com/lucky/around/meal/repository/RestaurantRepository.java
@@ -14,12 +14,16 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, String> 
           + "WHERE ST_Distance(ST_SetSRID(ST_MakePoint(r.lon, r.lat), 4326), ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) <= :range "
           + "ORDER BY ST_Distance(ST_SetSRID(ST_MakePoint(r.lon, r.lat), 4326), ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) ASC")
   List<Restaurant> findRestaurantsWithinRangeByDistance(
-      @Param("lat") double lat, @Param("lon") double lon, @Param("range") double range);
+      @Param("lat") final double lat,
+      @Param("lon") final double lon,
+      @Param("range") final double range);
 
   @Query(
       "SELECT r FROM Restaurant r "
           + "WHERE ST_Distance(ST_SetSRID(ST_MakePoint(r.lon, r.lat), 4326), ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) <= :range "
           + "ORDER BY r.ratingAverage DESC")
   List<Restaurant> findRestaurantsWithinRangeByRating(
-      @Param("lat") double lat, @Param("lon") double lon, @Param("range") double range);
+      @Param("lat") final double lat,
+      @Param("lon") final double lon,
+      @Param("range") final double range);
 }
