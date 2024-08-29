0b30f5d30fa15e7f0bc752dc2a2f94a367c81860
sung-silver <jinna0319@gmail.com>
2024-08-29 22:36:21 +0900

    [FEAT] RestaurantService에서 Repository 호출 시 Point를 넘길 수 있도록 변경

--- src/main/java/com/lucky/around/meal/service/RestaurantService.java
+++ src/main/java/com/lucky/around/meal/service/RestaurantService.java
@@ -3,9 +3,11 @@ package com.lucky.around.meal.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.lucky.around.meal.common.util.GeometryUtil;
 import com.lucky.around.meal.controller.dto.GetRestaurantsDto;
 import com.lucky.around.meal.entity.Restaurant;
 import com.lucky.around.meal.repository.RestaurantRepository;
@@ -17,16 +19,17 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class RestaurantService {
   private final RestaurantRepository restaurantRepository;
+  private final GeometryUtil geometryUtil;
 
   public List<GetRestaurantsDto> getRestaurantsWithinRange(
       final double lat, final double lon, final double range, final String sort) {
     List<Restaurant> restaurants;
-
+    Point location = geometryUtil.createPoint(lat, lon);
     if ("rating".equalsIgnoreCase(sort)) {
-      restaurants = restaurantRepository.findRestaurantsWithinRangeByRating(lat, lon, range * 1000);
+      restaurants = restaurantRepository.findRestaurantsWithinRangeByRating(location, range * 1000);
     } else {
       restaurants =
-          restaurantRepository.findRestaurantsWithinRangeByDistance(lat, lon, range * 1000);
+          restaurantRepository.findRestaurantsWithinRangeByDistance(location, range * 1000);
     }
 
     return restaurants.stream().map(GetRestaurantsDto::toDto).collect(Collectors.toList());
