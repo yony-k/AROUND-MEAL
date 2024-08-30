bdcf9cc2746fa684e1665e9cb5bcdf86131baad9
YeonHee Kim <kyh03179@gmail.com>
2024-08-30 11:08:52 +0900

    [REFACTOR] MemberRepository 주석 추가

--- src/main/java/com/lucky/around/meal/repository/MemberRepository.java
+++ src/main/java/com/lucky/around/meal/repository/MemberRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lucky.around.meal.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+  // 로그인 시 memberName으로 사용자 반환
   Optional<Member> findByMemberName(String memberName);
 
+  // memberName 중복 검증시 사용
   boolean existsByMemberName(String memberName);
 }
