3958260ce6fb95974601d918f74920dac446a606
SeoJeongYu <bbwest0709@gmail.com>
2024-08-31 20:26:47 +0900

    [FEAT] 실시간 회원 위치 정보 레디스 조회

--- src/main/java/com/lucky/around/meal/common/redis/RedisRepository.java
+++ src/main/java/com/lucky/around/meal/common/redis/RedisRepository.java
@@ -1,11 +1,16 @@
 package com.lucky.around.meal.common.redis;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.lucky.around.meal.exception.CustomException;
+import com.lucky.around.meal.exception.exceptionType.MemberExceptionType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,5 +24,19 @@ public class RedisRepository {
     GeoOperations<String, String> geoOps = redisTemplate.opsForGeo();
     geoOps.add(key, new Point(lat, lon), String.valueOf(memberId));
     redisTemplate.expire(key, 15, TimeUnit.MINUTES);
+  }
+
+  public Point getRealTimeLocation(String key, Long memberId) {
+    GeoOperations<String, String> geoOps = redisTemplate.opsForGeo();
+    List<Point> positions = geoOps.position(key, String.valueOf(memberId));
+
+    return getFirstPositionOrThrow(positions);
+  }
+
+  private Point getFirstPositionOrThrow(List<Point> positions) {
+    return Optional.ofNullable(positions)
+        .filter(list -> !list.isEmpty())
+        .map(list -> list.get(0))
+        .orElseThrow(() -> new CustomException(MemberExceptionType.REALTIME_LOCATION_NOT_FOUND));
   }
 }
