bee675848f60d6272f48ec0d95abcac5e39bab13
Sona <objet_an@naver.com>
2024-08-31 21:06:27 +0900

    [REFACTOR] 시군구 조회 record 변환

--- src/main/java/com/lucky/around/meal/controller/RegionController.java
+++ src/main/java/com/lucky/around/meal/controller/RegionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lucky.around.meal.entity.Region;
+import com.lucky.around.meal.controller.record.RegionRecord;
 import com.lucky.around.meal.service.RegionService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,7 @@ public class RegionController {
 
   // 시군구 전체 목록 조회
   @GetMapping("/allRegionList")
-  public ResponseEntity<List<Region>> getAllRegionList() {
+  public ResponseEntity<List<RegionRecord>> getAllRegionList() {
     return ResponseEntity.ok(regionService.getAllRegionList());
   }
 }
