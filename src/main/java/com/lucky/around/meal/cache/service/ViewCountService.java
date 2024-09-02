542de1bd340cf4ce1a4923f36873f5f4db8aad13
SeoJeongYu <bbwest0709@gmail.com>
2024-09-02 09:44:12 +0900

    [FEAT] 특정 맛집의 조회수 가져오기

--- src/main/java/com/lucky/around/meal/cache/service/ViewCountService.java
+++ src/main/java/com/lucky/around/meal/cache/service/ViewCountService.java
@@ -22,4 +22,12 @@ public class ViewCountService {
     ValueOperations<String, String> ops = redisTemplate.opsForValue();
     ops.increment(key, 1);
   }
+
+  // 특정 맛집의 조회수 가져오기
+  public Long getViewCount(String restaurantId) {
+    String key = buildKey(restaurantId);
+    ValueOperations<String, String> ops = redisTemplate.opsForValue();
+    String value = ops.get(key);
+    return value != null ? Long.parseLong(value) : 0L;
+  }
 }
