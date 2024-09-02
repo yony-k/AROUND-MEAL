f75a656a410cb596dcff9553078a54efc0d4a244
SeoJeongYu <bbwest0709@gmail.com>
2024-09-02 09:28:26 +0900

    [ADD] LocalDateTime 필드 JSON 직렬화 및 역직렬화 설정 추가

--- src/main/java/com/lucky/around/meal/controller/response/RatingResponseDto.java
+++ src/main/java/com/lucky/around/meal/controller/response/RatingResponseDto.java
@@ -2,10 +2,18 @@ package com.lucky.around.meal.controller.response;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.*;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 public record RatingResponseDto(
     Long ratingId,
     Long memberId,
     String restaurantId,
     Integer score,
     String content,
-    LocalDateTime createAt) {}
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+        LocalDateTime createAt) {}
