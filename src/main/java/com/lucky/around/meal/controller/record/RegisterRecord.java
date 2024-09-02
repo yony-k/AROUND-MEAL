0c9463b5b187312374aa5241489a4cf7d7768e85
sung-silver <jinna0319@gmail.com>
2024-09-02 23:28:11 +0900

    [FEAT] 회원가입 시 사용되는 RegisterRecord에 Member 엔티티에 추가된 필드 반영

--- src/main/java/com/lucky/around/meal/controller/record/RegisterRecord.java
+++ src/main/java/com/lucky/around/meal/controller/record/RegisterRecord.java
@@ -3,9 +3,14 @@ package com.lucky.around.meal.controller.record;
 import com.lucky.around.meal.entity.Member;
 import com.lucky.around.meal.entity.enums.MemberRole;
 
-public record RegisterRecord(String memberName, String password, String email) {
-
+public record RegisterRecord(
+    String memberName,
+    String password,
+    String email,
+    String webhookUrl,
+    boolean lunchRecommendAgree) {
   public Member toMember(String password) {
+
     return Member.builder()
         .memberName(memberName())
         .password(password)
@@ -13,6 +18,8 @@ public record RegisterRecord(String memberName, String password, String email) {
         .lat(0)
         .lon(0)
         .role(MemberRole.USER)
+        .memberWebhookUrl(webhookUrl())
+        .launchRecommendAgree(lunchRecommendAgree())
         .build();
   }
 }
