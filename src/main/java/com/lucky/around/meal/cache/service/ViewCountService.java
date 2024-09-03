40f3081aebaa40b8c877eadcfd5f73317aece335
SeoJeongYu <bbwest0709@gmail.com>
2024-09-03 09:34:54 +0900

    [FEAT] 1시간 기준 조회수 N개 이상 캐싱처리

--- src/main/java/com/lucky/around/meal/cache/service/ViewCountService.java
+++ src/main/java/com/lucky/around/meal/cache/service/ViewCountService.java
@@ -1,12 +1,22 @@
 package com.lucky.around.meal.cache.service;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.data.redis.core.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lucky.around.meal.controller.response.RestaurantDetailResponseDto;
+import com.lucky.around.meal.entity.Restaurant;
+import com.lucky.around.meal.exception.CustomException;
+import com.lucky.around.meal.exception.exceptionType.RestaurantExceptionType;
 import com.lucky.around.meal.repository.RestaurantRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -26,12 +36,11 @@ public class ViewCountService {
   private static final String RESTAURANT_DETAIL_KEY_PREFIX = "restaurantDetail::";
   private static final long VIEW_COUNT_THRESHOLD = 10L;
 
-  //
-  //    @PostConstruct
-  //    public void init() {
-  //        log.error("조회수 기준 캐싱 작업 에러");
-  //        updateHourlyViewCountCache();
-  //    }
+  @PostConstruct
+  public void init() {
+    log.error("조회수 기준 캐싱 작업 에러");
+    updateHourlyViewCountCache();
+  }
 
   private String buildKey(String prefix, String restaurantId) {
     return prefix + restaurantId;
@@ -65,6 +74,61 @@ public class ViewCountService {
     return value != null ? Long.parseLong(value) : 0L;
   }
 
+  // 1시간 기준 조회수 N개 이상 캐싱처리
+  @Scheduled(cron = "0 0 * * * ?") // 매시 정각에 실행
+  public void updateHourlyViewCountCache() {
+    Set<String> hourlyKeys = redisTemplate.keys(HOURLY_VIEW_COUNT_KEY_PREFIX + "*");
+    if (hourlyKeys != null) {
+      Map<String, Long> hourlyViewCounts = getHourlyViewCounts(hourlyKeys);
+      List<String> highViewRestaurantIds = filterHighViewRestaurants(hourlyViewCounts);
+
+      if (!highViewRestaurantIds.isEmpty()) {
+        cacheHighViewRestaurants(highViewRestaurantIds);
+      }
+    }
+  }
+
+  // 조회수 맵 생성
+  private Map<String, Long> getHourlyViewCounts(Set<String> keys) {
+    return keys.stream()
+        .collect(
+            Collectors.toMap(
+                key -> key.replace(HOURLY_VIEW_COUNT_KEY_PREFIX, ""),
+                key -> Long.parseLong(redisTemplate.opsForValue().get(key))));
+  }
+
+  // 조회수 기준에 맞는 맛집 필터
+  private List<String> filterHighViewRestaurants(Map<String, Long> viewCounts) {
+    return viewCounts.entrySet().stream()
+        .filter(entry -> entry.getValue() >= VIEW_COUNT_THRESHOLD)
+        .map(Map.Entry::getKey)
+        .toList();
+  }
+
+  // 조회수가 기준에 충족하는 맛집
+  private void cacheHighViewRestaurants(List<String> restaurantIds) {
+    for (String restaurantId : restaurantIds) {
+      Restaurant restaurant = getRestaurantById(restaurantId);
+      RestaurantDetailResponseDto dto = mapRestaurantToDto(restaurant);
+      cacheRestaurantDetail(restaurantId, dto);
+    }
+  }
+
+  private Restaurant getRestaurantById(String restaurantId) {
+    return restaurantRepository
+        .findById(restaurantId)
+        .orElseThrow(() -> new CustomException(RestaurantExceptionType.RESTAURANT_NOT_FOUND));
+  }
+
+  private void cacheRestaurantDetail(String restaurantId, RestaurantDetailResponseDto dto) {
+    try {
+      String json = objectMapper.writeValueAsString(dto);
+      redisTemplate.opsForValue().set(buildKey(RESTAURANT_DETAIL_KEY_PREFIX, restaurantId), json);
+    } catch (JsonProcessingException e) {
+      log.error("Json 직렬화 문제", e);
+    }
+  }
+
   // 조회수 초기화
   @Scheduled(cron = "0 0 11,17 * * ?") // 매일 오전 11시와 오후 5시에 실행
   public void resetViewCounts() {
@@ -74,5 +138,21 @@ public class ViewCountService {
         redisTemplate.opsForValue().set(key, "0");
       }
     }
+  }
+
+  private RestaurantDetailResponseDto mapRestaurantToDto(Restaurant restaurant) {
+    return new RestaurantDetailResponseDto(
+        restaurant.getId(),
+        restaurant.getRestaurantName(),
+        restaurant.getDosi(),
+        restaurant.getSigungu(),
+        restaurant.getJibunDetailAddress(),
+        restaurant.getDoroDetailAddress(),
+        restaurant.getCategory().name(),
+        restaurant.getRestaurantTel(),
+        restaurant.getLocation().getX(),
+        restaurant.getLocation().getY(),
+        restaurant.getRatingAverage(),
+        List.of());
   }
 }
